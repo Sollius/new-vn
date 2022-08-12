dabf936d7b5dfe89e9d997f204849c87751b4ade
Sollius <sollius@mail.ru>
2022-08-13 02:13:27 +0300

    fix tabs

--- new vn/Menu.h
+++ new vn/Menu.h
@@ -5,14 +5,14 @@
 class Menu
 {
 private:
-	int m_itemsCount = 0;
-	Vector2f m_startPoint = Vector2f(0.f, 0.f);
-	Vector2f m_size = Vector2f(0.f, 0.f);
-	RectangleShape m_mainShape = RectangleShape(Vector2f(0.f, 0.f));
-	std::vector<MenuItem> m_items = std::vector<MenuItem>(size_t(0));
-	Font m_font = Font();
-	int m_fontSize = 20;
-	std::string m_alignment = "left";
+	int						m_itemsCount	= 0;
+	Vector2f				m_startPoint	= Vector2f(0.f, 0.f);
+	Vector2f				m_size			= Vector2f(0.f, 0.f);
+	RectangleShape			m_mainShape		= RectangleShape(Vector2f(0.f, 0.f));
+	std::vector<MenuItem>	m_items			= std::vector<MenuItem>(size_t(0));
+	Font					m_font			= Font();
+	int						m_fontSize		= 20;
+	std::string				m_alignment		= "left";
 
 public:
 	Menu();
